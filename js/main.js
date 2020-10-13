75502569250950b499b1d9ad6e569e054074b337
valera02surkov <AKIG1@list.ru>
2020-10-13 22:25:04 +0500

    creted: advant + адаптив

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   var openModal = document.querySelector(".header__mobail");
   openModal.addEventListener("click", function () {
-    console.log("клик");
     document
       .querySelector(".modal-header")
-      .classList.toggle("modal-header--visible")
+      .classList.toggle("modal-header--visible")  
   });
+
   var closeModalButton = $(".modal__close")
   var modalHeader = $(".modal-header")
   modalHeader.on("click", openModal);
@@ -14,7 +14,7 @@ $(document).ready(function () {
   function openModal() { 
     modalHeader.addClass("modal-header--visible")
   };
-  function closeModal(event) {
+  function closeModal() {
      modalHeader.removeClass("modal-header--visible")
-  }
+  };
 });
